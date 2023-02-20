a5f2f4a851cca2fd50174b959469533288714631
GrassyAirplane <euanlimcs@gmail.com>
2023-02-20 16:20:35 +0800

    website

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,6 +2,13 @@ const Navbar = () => {
     return (
         <>
             <h1>Euan</h1>
+            <ul>
+                <li>About</li>
+                <li>Experience </li>
+                <li>Projects </li>
+                <li>Contact </li>
+            </ul>
+            <button>Resume</button>
         </>
     )
 }
