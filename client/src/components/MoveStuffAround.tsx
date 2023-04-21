485b5109117ca9c072530632c7ce91b2183278fd
GrassyAirplane <euanlimcs@gmail.com>
2023-04-21 08:53:11 +0800

    removal of react-ticker

--- client/src/components/MoveStuffAround.tsx
+++ client/src/components/MoveStuffAround.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 // import Ticker from 'react-ticker'
 
-const MoveStuffAround = () => (
-    // <Ticker>
-    //     {({ index }) => (
-    //         <>
-    //             <h1>This is the Headline of element #{index}!</h1>
-
-    //         </>
-    //     )}
-    // </Ticker>
-)
+const MoveStuffAround = () => {
+    return (
+        <>
+        </>
+    )
+}
 
 export default MoveStuffAround
